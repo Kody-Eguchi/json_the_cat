2596cc62acba1aa8139d44f784988d8b29376da7
Koji Eguchi <koji.eguchi@hotmail.com>
2023-02-16 10:24:31 -0500

    added a function to take a commandline argument as query

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,24 @@
 const request = require('request');
+const args = process.argv.slice(2);
+const name = args[0];
 
-const url = 'https://api.thecatapi.com/v1/breeds/search?q=sib';
+// const url = 'https://api.thecatapi.com/v1/breeds/search?q=sib';
 
-const breedFetcher = function(url) {
-  request(url, (err, res, body) => {
+const breedFetcher = function(name) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${name}`, (err, res, body) => {
+    if (err) {
+      console.log(`REQUEST FAILED - ERROR MESSAGE: ${err}`);
+      return;
+    }
     const data = JSON.parse(body);
-    console.log(data);
-    console.log(typeof data);
+    if (data.length === 0) {
+      console.log(`Breed of ${name} is no found`);
+      return;
+    }
+    console.log(data[0].description);
+    return;
   });
 };
 
-breedFetcher(url);
+breedFetcher(name);
+
