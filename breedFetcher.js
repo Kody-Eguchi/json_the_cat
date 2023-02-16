8daf2589356f8d2a530906c5f0e62d285019419a
Koji Eguchi <koji.eguchi@hotmail.com>
2023-02-16 13:52:15 -0500

    refactored

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,22 @@
 const request = require('request');
-const args = process.argv.slice(2);
-const name = args[0];
 
-// const url = 'https://api.thecatapi.com/v1/breeds/search?q=sib';
-
-const breedFetcher = function(name) {
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${name}`, (err, res, body) => {
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (err, res, body) => {
     if (err) {
-      console.log(`REQUEST FAILED - ERROR MESSAGE: ${err}`);
+      callback(err, null);
       return;
     }
+
     const data = JSON.parse(body);
+
     if (data.length === 0) {
-      console.log(`Breed of ${name} is no found`);
+      callback(`Breed of ${breedName} is not found`, null);
       return;
     }
-    console.log(data[0].description);
+    
+    callback(null, data[0].description);
     return;
   });
 };
 
-breedFetcher(name);
-
+module.exports = {fetchBreedDescription};
